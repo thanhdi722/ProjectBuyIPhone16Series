5000eda433d4191d56ddd7bdfdb961dfd9f1c1bd
thanhdi721 <thanhdi721@gmail.com>
2024-09-24 18:33:46 +0700

    update

--- src/Component/InfoTechnicalComponent/InfoTechnicalComponent.tsx
+++ src/Component/InfoTechnicalComponent/InfoTechnicalComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import "./infoTechnicalComponent.css";
+import "./infoStyle.css";
 import Image from "next/image";
 import { StaticImageData } from "next/image";
 import Link from "next/link";
