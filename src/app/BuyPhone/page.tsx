b0cb813b8e24b6d727d38e640c4bc0fff5c34ac1
CarsonDev1 <buitritinht@gmail.com>
2024-09-24 11:45:01 +0700

    upload code

--- src/app/BuyPhone/page.tsx
+++ src/app/BuyPhone/page.tsx
@@ -5,12 +5,16 @@ import Ecosystem from "@/Component/Ecosystem";
 import InfoTechnical from "../../Component/BuyPhoneComponent/InfoTechnical/InfoTechnical";
 // import PrioritizePage from "../../Component/BuyPhoneComponent/prioritizePage/prioritizePage";
 import ImagesInfo from "../../Component/BuyPhoneComponent/ImagesInfo/ImagesInfo";
+import Image from "next/image";
 function page() {
   return (
     <div className="bg-page">
       <HeaderConponent />
       <InfoTechnical />
       {/* <PrioritizePage /> */}
+      <div className="container">
+        <Image src='/thuculendoi.webp' width={1820} height={1200} alt="thu-cu-len-doi" />
+      </div>
       <UpgradeList />
       <Ecosystem />
       <ImagesInfo />
